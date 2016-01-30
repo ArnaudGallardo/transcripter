9156cdd32e8d12b65ac3d5995033791cf3a8b779
mokolodi1 <mokolodi1@gmail.com>
2016-01-30 12:28:00 -0800

    added schemas

--- speeching/lib/collections.js
+++ speeching/lib/collections.js
@@ -1,1 +1,28 @@
+Meetings = new Meteor.Collection("meetings");
+Meetings.attachSchema(new SimpleSchema({
+  leaderUserId: { type: [Meteor.ObjectID] },
+  users: {
+    type: [
+      new SimpleSchema({
+        userId: { type: Meteor.ObjectID },
+        color: { type: String },
+      }),
+    ],
+  },
+  recordingState: {
+    type: String,
+    allowedValues: [
+      "recording",
+      "paused",
+      "ended",
+    ],
+  },
+}));
+
 Messages = new Meteor.Collection("messages");
+Messages.attachSchema(new SimpleSchema({
+  userId: { type: Meteor.ObjectID },
+  username: { type: String },
+  meetingId: { type: Meteor.ObjectID },
+  text: { type: String },
+}));
